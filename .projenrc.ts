7c1326a86ef00dbb2800f15871b84ea70cee77fa
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-11-02 19:56:44 +0100

    feat(projen): migrate to projen-extensions (#3)
    * feat(projen): migrate to projen-extensions
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,38 +1,30 @@
-import { AwsCdkTypeScriptApp } from 'projen/lib/awscdk';
-import { GithubCredentials } from 'projen/lib/github';
-import { UpgradeDependenciesSchedule } from 'projen/lib/javascript';
+import { TypescriptApplicationProject } from '@gplassard/projen-extensions';
 
-const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
+const project = new TypescriptApplicationProject({
   name: 'aws-tagging-metrics',
-  projenrcTs: true,
-  githubOptions: {
-    mergify: false,
-    projenCredentials: GithubCredentials.fromPersonalAccessToken({ secret: 'GITHUB_TOKEN' }),
-  },
-  gitignore: ['*.iml', '.idea', '.vscode'],
-  sampleCode: false,
-  jestOptions: {
-    configFilePath: 'jest.config.json',
-  },
-  depsUpgradeOptions: {
-    workflowOptions: {
-      labels: ['dependencies'],
-      schedule: UpgradeDependenciesSchedule.MONTHLY,
-    },
-  },
   deps: [
-    '@aws-sdk/client-resource-groups-tagging-api', '@aws-lambda-powertools/metrics', '@aws-lambda-powertools/logger', 'lodash',
+    '@aws-sdk/client-resource-groups-tagging-api', '@aws-lambda-powertools/metrics', '@aws-lambda-powertools/logger', 'lodash', 'aws-cdk-lib', 'constructs',
   ],
   devDeps: [
-    '@types/aws-lambda', '@types/lodash',
+    '@types/aws-lambda', '@types/lodash', 'aws-cdk', 'esbuild',
   ],
   scripts: {
+    'cdk:deploy': 'yarn cdk --app "ts-node ./src/main.ts" deploy',
+    'cdk:diff': 'yarn cdk --app "ts-node ./src/main.ts" diff',
+    'cdk:synth': 'yarn cdk --app "ts-node ./src/main.ts" synth',
+    'cdk:destroy': 'yarn cdk --app "ts-node ./src/main.ts" destroy',
     'cdk:local:deploy': 'yarn cdk --app "ts-node ./src/local-deploy.ts" deploy',
     'cdk:local:diff': 'yarn cdk --app "ts-node ./src/local-deploy.ts" diff',
     'cdk:local:synth': 'yarn cdk --app "ts-node ./src/local-deploy.ts" synth',
     'cdk:local:destroy': 'yarn cdk --app "ts-node ./src/local-deploy.ts" destroy',
   },
+  gitignore: [...TypescriptApplicationProject.DEFAULT_GITIGNORE, 'cdk.out'],
+  tsconfig: {
+    compilerOptions: {
+      skipLibCheck: true,
+    },
+    exclude: ['cdk.out'],
+  },
 });
+project.addPackageIgnore('cdk.out');
 project.synth();
